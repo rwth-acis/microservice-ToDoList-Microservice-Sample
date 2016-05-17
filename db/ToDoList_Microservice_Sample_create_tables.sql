4726d05d161887a1834379a3480b6167862f3ee4
CAEUser <CAEAddress@gmail.com>
2016-05-18 01:36:15 +0200

    Generated microservice version 0.1

---
+++ db/ToDoList_Microservice_Sample_create_tables.sql
@@ -0,0 +1,14 @@
+--
+-- Database Schema:  todolist 
+-- Automatically generated sql script for the service ToDoList Microservice Sample, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table list.
+--
+CREATE TABLE todolist.list (
+  id int,
+  ListContent TEXT,
+  CONSTRAINT id_PK PRIMARY KEY (id)
+);
+
